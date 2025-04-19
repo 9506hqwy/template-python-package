701d5f3fd64ba1c1098ecb7de4cd0049b5d0288f
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2025-04-19 23:09:16 +0900

    環境を更新した。

--- .devcontainer-compose/post-create.sh
+++ .devcontainer-compose/post-create.sh
@@ -5,10 +5,5 @@ sudo apt-get update
 sudo apt-get install postgresql-client -y
 
 python -m pip install --upgrade pip
-
-pip install pytest pytest-cov
-
-curl -LsSf https://astral.sh/uv/install.sh | sh
-export PATH="$PATH:$HOME/.local/bin"
-
+pip install uv
 uv sync --all-groups
